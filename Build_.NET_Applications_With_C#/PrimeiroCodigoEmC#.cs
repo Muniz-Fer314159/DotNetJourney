0a87f1abec8f905c53e2a0dfa283598042932bcf
Muniz-Fer314159 <munizmurilo797@gmail.com>
2025-08-15 04:59:32 -0300

    Inicio da seção 1 da coleção do Microsoft Learn. Write your first C# code e Introduction to .NET completos.

---
+++ Build_.NET_Applications_With_C#/PrimeiroCodigoEmC#.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Hello Word");
+
+Console.Write("Congratulations");
+Console.Write(" ");
+Console.Write("You have completed the first step.");
+
+//Diferença entre Console.Write e Console.WriteLine
+//O Console.Write imprime uma linha sem salto de linha
+//enquanto o Console.WriteLine imprime uma linha com salto de linha.
